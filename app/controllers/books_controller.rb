4ef976223e3301a83d696747626ad1ad7e738650
EC2 Default User <ogmegumi@gmail.com>
2021-06-08 16:29:45 +0000

    [Update]3-8 バリテーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,8 @@ class BooksController < ApplicationController
     def create
       @book = Book.new(book_params)
       if @book.save
-        flash[:notice] = "Book was successfully created."
-        redirect_to book_path(@book.id)
+         flash[:notice] = "Book was successfully created."
+         redirect_to book_path(@book.id)
       else
         @books = Book.all
         render 'index'
@@ -26,18 +26,19 @@ class BooksController < ApplicationController
     def update
       @book = Book.find(params[:id])
       if @book.update(book_params)
-       flash[:notice] = "Book was successfully updated."
-       redirect_to book_path(@book)
-     else
-       render 'edit'
-     end
+         flash[:notice] = "Book was successfully updated."
+         redirect_to book_path(@book)
+      else
+        @books = Book.all
+        render 'edit'
+      end
     end
 
     def destroy
       book = Book.find(params[:id])
       if book.destroy
-        flash[:notice] = "Book was successfully destroyed"
-        redirect_to books_path
+         flash[:notice] = "Book was successfully destroyed. "
+         redirect_to books_path(book.id)
       end
     end
 
