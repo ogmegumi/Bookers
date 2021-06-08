c439f70cbe1be506ac6cffd61e6711d4370a6f58
EC2 Default User <ogmegumi@gmail.com>
2021-06-08 04:19:54 +0000

    [Update]edit機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,29 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
