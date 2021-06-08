2d21aec061f7a1d42e59d553422a526eb1aaa440
EC2 Default User <ogmegumi@gmail.com>
2021-06-08 08:37:25 +0000

    [Update]削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,51 @@
 class BooksController < ApplicationController
-  def index
-    @book = Book.new
-    @books = Book.all
-  end
+    def index
+      @book = Book.new
+      @books = Book.all
+    end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
-  end
+    def create
+      book = Book.new(book_params)
+      if @book
+        book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(book.id)
+      else
+        flash.now[:alert] = "error:Book wasn't created"
+        @books = Book.all
+        render 'index'
+      end
+    end
+    
+    def show
+      @book = Book.find(params[:id])
+    end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+    def edit
+      @book = Book.find(params[:id])
+    end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
+    def update
+      book = Book.find(params[:id])
+      if book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(book)
+     else
+       flash.now[:alert] = "error:Book wasn't updated"
+       @book = Book.all
+       render 'edit'
+     end
+    end
 
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-  end
+    def destroy
+      book = Book.find(params[:id])
+      if book.destroy
+        flash[:notice] = "Book was successfully destroyed"
+        redirect_to books_path
+      else
+        flash.now[:alert] = "error:Book wasn't created"
+      end
+    end
 
   private
   def book_params
